cef55ce909744a5e5c48e479f32a48aa11796851
rolf carlsen <rolfcarlsen@rolfs-MacBook-Pro.local>
2025-09-02 11:56:18 +0200

    added user database

---
+++ sql/queries/strava_users.sql
@@ -0,0 +1,19 @@
+-- name: CreateStravaUser :one
+INSERT INTO STRAVA_USER (id, user_id, strava_id, refresh_token, refresh_token_expires_at,
+    created_at,updated_at, username, firstname, lastname, city, state, country, sex,
+    premuim, weight)
+VALUES (
+    gen_random_uuid(),
+    $1,$2,$3,$4,$5,NOW(),NOW(),$6,$7,$8,$9,$10,$11,$12,$13 
+)
+RETURNING  *; 
+
+-- name: GetStravaRefreshToken :one
+SELECT refresh_token,refresh_token_expires_at FROM STRAVA_USER
+WHERE user_id = $1; 
+
+-- name: UpdateStravaRefreshToken :exec
+UPDATE STRAVA_USER
+SET refresh_token = $2,
+    refresh_token_expires_at = $3
+Where user_id=$1;  
