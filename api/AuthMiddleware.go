0b30d43bedbff5993b6af22f23d3afd2bc9d7e7c
rolf carlsen <rolfcarlsen@rolfs-MacBook-Pro.local>
2025-09-29 11:12:04 +0200

    fixed statichedk and added gcloud admin api

--- api/AuthMiddleware.go
+++ api/AuthMiddleware.go
@@ -28,7 +28,10 @@ func (cfg *APIConfig) AuthMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "userID", userID)
+		type userContextID string
+		const userIDKey userContextID = "userID"
+
+		ctx := context.WithValue(r.Context(), userIDKey, userID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 
 	})
